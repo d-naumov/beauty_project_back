09890fd3781609511a1086adfa21d384b861a18b
d-naumov <nd76@gmx.de>
2024-03-02 21:21:20 +0100

    add: add info.txt

--- src/main/java/com/example/end/config/SwaggerConfig.java
+++ src/main/java/com/example/end/config/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
   public Docket api() {
     return new Docket(DocumentationType.SWAGGER_2)
         .select()
-        .apis(RequestHandlerSelectors.basePackage("com.yourcompany.yourproject.controller"))
+        .apis(RequestHandlerSelectors.basePackage("com.example.end.controller"))
         .paths(PathSelectors.any())
         .build();
   }
