a99ec3c26829656c0077999900452a7e2deadea0
Natalja Röhm <roehm.natalia@gmail.com>
2024-03-23 21:02:11 +0100

    add category

--- src/main/java/com/example/end/mapping/CategoryMapper.java
+++ src/main/java/com/example/end/mapping/CategoryMapper.java
@@ -2,6 +2,7 @@ package com.example.end.mapping;
 
 import com.example.end.dto.CategoryDto;
 import com.example.end.models.Category;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
@@ -9,5 +10,6 @@ public interface CategoryMapper {
 
     CategoryDto toDto(Category category);
 
+    @InheritInverseConfiguration
     Category toEntity(CategoryDto categoryDto);
 }
