07d2684ccee426be1c94bcb1db9c1588e9729718
d-naumov <nd76@gmx.de>
2024-04-04 20:42:30 +0200

    a

--- src/main/java/com/example/end/BeautyProjectApplication.java
+++ src/main/java/com/example/end/BeautyProjectApplication.java
@@ -2,11 +2,8 @@ package com.example.end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class BeautyProjectApplication {
 
 	public static void main(String[] args) {
