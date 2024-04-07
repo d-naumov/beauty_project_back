821aa124e3d6aff64f91dc20a6e2c202dfe530b8
d-naumov <nd76@gmx.de>
2024-04-07 18:45:16 +0200

    a

--- src/main/java/com/example/end/controller/AuthenticationController.java
+++ src/main/java/com/example/end/controller/AuthenticationController.java
@@ -1,8 +1,10 @@
 package com.example.end.controller;
 
-import com.example.end.dto.NewUserDto;
+import com.example.end.dto.LoginRequestDto;
+
 import com.example.end.dto.UserDto;
 import com.example.end.service.interfaces.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,9 +19,11 @@ public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
 
+    @Operation(summary = "Authenticate user", description = "Authenticate a user with the provided email and password.")
     @PostMapping("/login")
-    public ResponseEntity<UserDto> loginUser(@RequestBody NewUserDto newUserDto) {
-        UserDto userDto = authenticationService.authenticate(newUserDto.getEmail(), newUserDto.getHashPassword());
+    public ResponseEntity<UserDto> loginUser(@RequestBody LoginRequestDto loginRequestDto) {
+        UserDto userDto = authenticationService.authenticate(loginRequestDto.getEmail(),
+                loginRequestDto.getHashPassword());
         return ResponseEntity.ok(userDto);
     }
 }
