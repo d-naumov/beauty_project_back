a024cc16636ca0ac9327db49624b4d3ef6746544
Aleksandra Cheidze <a.cxeidze@gmail.com>
2024-04-29 15:07:21 +0200

    fix findProceduresByCategoryId

--- src/main/java/com/example/end/repository/ProcedureRepository.java
+++ src/main/java/com/example/end/repository/ProcedureRepository.java
@@ -4,6 +4,7 @@ package com.example.end.repository;
 import com.example.end.models.Procedure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,5 +12,5 @@ import java.util.Optional;
 public interface ProcedureRepository extends JpaRepository<Procedure, Long> {
     Optional<Procedure> findByName(String name);
     @Query("SELECT p FROM Procedure p JOIN p.category c WHERE c.id = :categoryId")
-    List<Procedure> findProceduresByCategoryId(Long categoryId);
+    List<Procedure> findProceduresByCategoryId(@Param("categoryId") Long categoryId);
 }
